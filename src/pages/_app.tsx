0e54e1f2d0e2aced7f64a7370f2004e6ac9276a9
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-10-08 21:17:42 +0200

    demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,15 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex flex-col w-screen min-h-screen p-4">
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </div>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,15 +1,50 @@
 import { t } from "../trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 export const exampleRouter = t.router({
-  hello: t.procedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
+  create: t.procedure
+    .input(
+      z.object({
+        id: z.string().min(1),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const post = await ctx.prisma.example.create({
+        data: {
+          id: input.id,
+        },
+      });
+
+      return post;
     }),
-  getAll: t.procedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+
+  getPaginated: t.procedure
+    .input(z.object({ foo: z.string(), cursor: z.string().nullish() }))
+    .query(async ({ ctx, input }) => {
+      const { cursor } = input;
+
+      const posts = await ctx.prisma.example.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 3,
+        cursor: cursor ? { id: cursor } : undefined,
+      });
+
+      if (!Array.isArray(posts)) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Error getting posts",
+        });
+      }
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (posts.length > 3) {
+        const nextItem = posts.pop();
+        nextCursor = nextItem?.id || "";
+      }
+
+      return { posts, nextCursor };
+    }),
 });
